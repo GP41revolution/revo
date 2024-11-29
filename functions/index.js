9551f94bccbfba7ede8174b86363115ec097e115
shirohacha <shimadak2003@gmail.com>
2024-11-29 14:23:56 +0900

    index.htmlを初期化とESlintErrorを対処と不要なファイル削除

--- functions/index.js
+++ functions/index.js
@@ -10,10 +10,12 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
+logger.info("This is a test log message.");
+
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
